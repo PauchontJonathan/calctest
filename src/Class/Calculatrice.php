3d42e6b0fb47f063415e6d973a050d98535bb801
jonathan <jonathan.pauchont@livecampus.tech>
2022-08-02 14:03:55 +0200

    wip

---
+++ src/Class/Calculatrice.php
@@ -0,0 +1,76 @@
+<?php
+class Calculatrice {
+    /**
+     * preCall
+     *
+     * @param  string $method
+     * @param  float $a
+     * @param  float $b
+     * @return void
+     */
+    public function preCall(string $method, float $a, float $b): float
+    {
+        $methods = [
+            '+' => 'add',
+            '-' => 'minus',
+            '*' => 'multiply',
+            '/' => 'divide'
+        ];
+
+        $currentMethod = $methods[$method];
+        return $this->$currentMethod($a, $b);
+    }
+
+    /**
+     * add
+     *
+     * @param  float $a
+     * @param  float $b
+     * @return float
+     */
+    private function add(float $a, float $b): float
+    {
+        return $a + $b;
+    }
+
+    /**
+     * sub
+     *
+     * @param  float $a
+     * @param  float $b
+     * @return float
+     */
+    private function sub(float $a, float $b): float
+    {
+        return $a - $b;
+    }
+
+    /**
+     * multiply
+     *
+     * @param  float $a
+     * @param  float $b
+     * @return float
+     */
+    private function multiply(float $a, float $b): float
+    {
+        return $a * $b;
+    }
+
+    /**
+     * divide
+     *
+     * @param  float $a
+     * @param  float $b
+     * @return float
+     */
+    private function divide(float $a, float $b): float
+    {
+        return $a / $b;
+    }
+}
+
+$calculate = new Calculatrice();
+$result = $calculate->preCall('+', 2, 5);
+
+echo $result;
---
+++ src/Test/CalculatriceTest.php
@@ -0,0 +1,7 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class CalculatriceTest extends TestCase
+{
+
+}
